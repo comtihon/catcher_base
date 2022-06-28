b1a9ba142d9eb8ed0cd6cf7b4c683b016547fe78
Val <valerii.tikhonov@gmail.com>
2022-06-28 15:00:52 +0200

    fix server version startup problem

--- src/main/kotlin/com/catcher/base/controller/SystemController.kt
+++ src/main/kotlin/com/catcher/base/controller/SystemController.kt
@@ -3,15 +3,16 @@ package com.catcher.base.controller
 import com.catcher.base.data.dto.SystemInfoDTO
 import com.catcher.base.service.tool.ToolService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.info.BuildProperties
 import org.springframework.web.bind.annotation.*
+
 
 @RestController
 @RequestMapping("/api/v1/system")
 class SystemController(@Autowired val toolService: ToolService) {
 
-    @Value("\${server.version:not available}")
-    private val version: String? = null
+    @Autowired
+    private val buildProperties: BuildProperties? = null
 
     @GetMapping
     fun getInfo(): SystemInfoDTO {
@@ -20,7 +21,7 @@ class SystemController(@Autowired val toolService: ToolService) {
                 pythonProvider = python.first,
                 pythonVersion = python.second,
                 catcher = toolService.catcherVersion(),
-                version = version!!)
+                version = buildProperties!!.version)
     }
 
     @PutMapping("/catcher/{id}")
